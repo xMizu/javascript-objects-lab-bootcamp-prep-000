8f971aa071edac36da0d9ed102b59b1dcdef5674
Anthony Wan <anthonywan00@gmail.com>
2019-04-11 04:08:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
+  delete object[key]
   return object;
 }
 
